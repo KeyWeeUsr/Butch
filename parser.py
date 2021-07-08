02e5f7f73a9932034791c527b4ba1d8fdec3ca36
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2021-07-09 01:15:52 +0200

    [ADD] Add percent-encapsulated variable parsing

--- parser.py
+++ parser.py
@@ -1,5 +1,7 @@
+import re
 from typing import Tuple
 from commands import Command
+from context import Context
 
 
 def parse(values: str) -> Tuple[Command, list]:
@@ -22,3 +24,28 @@ def clear_input(value: str) -> str:
     if set(value) in (set(""), set("\n"), set("\r\n"), set("\n\r")):
         return ""
     return value
+
+
+def parse_variables(values: str, ctx: Context):
+    out = []
+
+    for value in values:
+        new = None
+
+        delayed = re.findall(r"!(.*)!", value)
+        if delayed:
+            delayed = delayed[0]
+            new = ctx.get_variable(delayed)
+            if new:
+                out.append(new)
+                continue
+
+        normal = re.findall(r"%(.*)%", value)
+        if normal:
+            normal = normal[0]
+            new = ctx.get_variable(normal)
+            if new:
+                out.append(new)
+                continue
+        out.append(value)
+    return out
