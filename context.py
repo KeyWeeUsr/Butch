f5ab58cf1d5742d058debe6602aec5dc9feb17df
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2021-07-09 00:34:01 +0200

    [ADD] Add variables to Context

--- context.py
+++ context.py
@@ -3,8 +3,11 @@ from os import getcwd
 
 class Context:
     _cwd: str = None
+    _variables: dict = None
 
     def __init__(self, **kwargs):
+        self._variables = {}
+
         for key, val in kwargs.items():
             if key not in dir(Context):
                 raise Exception(f"Bad key: '{key}' (ignored)")
@@ -13,6 +16,14 @@ class Context:
     @property
     def cwd(self):
         return self._cwd
+
+    @property
+    def variables(self):
+        return self._variables
+
+    def set_variable(self, key, value):
+        self._variables[key] = value
+
 
 def get_context() -> dict:
     cwd = getcwd().replace("/", "\\")
