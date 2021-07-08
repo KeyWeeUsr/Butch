0bfb3a40507ace655a3f451a23760e7765e8e983
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2021-07-09 00:39:01 +0200

    [ADD] Add default variables for Context

--- context.py
+++ context.py
@@ -24,6 +24,40 @@ class Context:
     def set_variable(self, key, value):
         self._variables[key] = value
 
+    def _get_default_variables(self):
+        return {
+            "allusersprofile": None,
+            "appdata": None,
+            "clientname": None,
+            "commonprogramfiles": None,
+            "computername": None,
+            "comspec": None,
+            "devmgr_show_nonpresent_devices": None,
+            "fp_no_host_check": None,
+            "homedrive": None,
+            "homepath": None,
+            "logonserver": None,
+            "number_of_processors": None,
+            "os": None,
+            "path": None,
+            "pathext": None,
+            "processor_architecture": None,
+            "processor_identifier": None,
+            "processor_level": None,
+            "processor_revision": None,
+            "programfiles": None,
+            "prompt": None,
+            "sessionname": None,
+            "systemdrive": None,
+            "systemroot": None,
+            "temp": None,
+            "tmp": None,
+            "userdomain": None,
+            "username": None,
+            "userprofile": None,
+            "windir": None
+        }
+
 
 def get_context() -> dict:
     cwd = getcwd().replace("/", "\\")
