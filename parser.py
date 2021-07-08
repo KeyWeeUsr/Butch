a51913ceeb11b38c732e5535a7864c36f0fdb8cc
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2021-07-08 22:42:54 +0200

    [ADD] Add test for Command.ECHO

--- parser.py
+++ parser.py
@@ -5,6 +5,6 @@ from commands import Command
 def parse(values: str) -> Tuple[Command, list]:
     cmd, *params = values.split(" ")
 
-    if cmd == Command.ECHO:
+    if cmd == Command.ECHO.value:
         return (Command.ECHO, params)
     return (Command.UNKNOWN, [cmd])
--- test.py
+++ test.py
@@ -8,5 +8,13 @@ class Parser(TestCase):
         cmd = "-"
         self.assertEqual(parse(cmd), (Command.UNKNOWN, [cmd]))
 
+    def test_echo(self):
+        from parser import parse
+        from commands import Command
+        params = ["hello"]
+        cmd = f"echo {params[0]}"
+        self.assertEqual(parse(cmd), (Command.ECHO, params))
+
+
 if __name__ == "__main__":
     main()
