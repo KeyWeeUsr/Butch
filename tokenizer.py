10872f0e3db102bbb05b6134c1e04b658ea263ce
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2021-07-14 18:21:44 +0200

    [ADD] Add Argument abstraction token

--- tokenizer.py
+++ tokenizer.py
@@ -60,6 +60,17 @@ class Flag(Enum):
     QUOTE = auto()
 
 
+class Argument:
+    _value: str = ""
+
+    def __init__(self, value: str):
+        self._value = value
+
+    @property
+    def value(self):
+        return self._value
+
+
 class Command:
     _cmd: CommandType = None
     _name: str = ""
