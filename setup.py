049ee044863723fb1bc1e12c2760aff3e2b2607f
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2021-07-21 18:06:27 +0200

    [ADD] Automatically locate Python package in setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from butch import get_version
 
 KWARGS = dict(
     name="butch",
-    version=get_version()
+    version=get_version(),
+    packages=find_packages()
 )
 
 
