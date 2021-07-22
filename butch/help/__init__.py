9fb25cc2fd373886848b5a4d35189ef91eecba25
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2021-07-22 07:45:24 +0200

    [REF] Move help.py code to help/__init__.py

--- butch/help/__init__.py
+++ butch/help/__init__.py
@@ -1,1 +1,15 @@
-# empty
+"""
+Module for reading the help files from "help" folder and dumping the contents
+to STDOUT (or other provided buffer, if any).
+"""
+
+import sys
+from os.path import abspath, dirname, join
+from butch.commands import Command
+
+
+def print_help(cmd: Command, file=sys.stdout):
+    "Open a help file for passed Command and print it."
+    folder = dirname(abspath(__file__))
+    with open(join(folder, "help", f"{cmd.value.lower()}.txt")) as fdes:
+        print(fdes.read(), file=file)
