5ec7283e061d3ed07a6668030276e4eb626eca51
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2021-07-22 07:38:25 +0200

    [ADD] Include .bat and .out files into package

--- setup.py
+++ setup.py
@@ -4,7 +4,14 @@ from butch import get_version
 KWARGS = dict(
     name="butch",
     version=get_version(),
-    packages=find_packages()
+    packages=find_packages(),
+    package_data={
+        "": ["*.txt", "*.bat", "*.out"]
+    },
+    include_package_data=True,
+    exclude_package_data={
+        "": ["*.pyc"]
+    }
 )
 
 
