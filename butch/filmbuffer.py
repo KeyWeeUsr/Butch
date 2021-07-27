f7693b2a4eef5575ef29a3ecd3e4699cd0c50aed
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2021-07-27 18:25:23 +0200

    [REF] Lint filmbuffer.py

--- butch/filmbuffer.py
+++ butch/filmbuffer.py
@@ -1,18 +1,20 @@
 """
 Abstraction of looking at a string as if via a film strip.
 
-                                 film window
-                +--------------+--------------+--------------+
-================|==============|==============|==============|=================
-                |              |              |              |
-                |   previous   |    current   |     next     |
-                |              |              |              |
-================|==============|==============|==============|=================
-                +--------------+--------------+--------------+
+.. code::
+
+                                   film window
+                  +--------------+--------------+--------------+
+    ==============|==============|==============|==============|===============
+                  |              |              |              |
+                  |   previous   |    current   |     next     |
+                  |              |              |              |
+    ==============|==============|==============|==============|===============
+                  +--------------+--------------+--------------+
 """
 
 
-class FilmBuffer:
+class FilmBuffer:  # noqa: WPS214
     """
     Look on the flat text buffer/string as if it's a movie film strip.
 
@@ -20,7 +22,7 @@ class FilmBuffer:
     look-before access.
     """
 
-    _data: str
+    _data: str  # noqa: WPS110
     _data_len: int
     _pos: int
     _last_pos: int
@@ -30,8 +32,15 @@ class FilmBuffer:
     _char: str
     _nchar: str
 
-    def __init__(self, data: str, start: int = 0):
-        self._data = data
+    def __init__(self, data: str, start: int = 0):  # noqa: WPS110
+        """
+        Initialize a FilmBuffer instance.
+
+        Args:
+            data (str): string to look over
+            start (int): start position of the film (defaults to 0).
+        """
+        self._data = data  # noqa: WPS110
         data_len = len(data)
         self._data_len = data_len
         self._last_pos = data_len - 1
@@ -45,35 +54,71 @@ class FilmBuffer:
 
     @property
     def pos(self):
-        "Property: current position of the film window."
+        """
+        Get the current position of the film window's center.
+
+        Returns:
+            current position of the film window.
+        """
         return self._pos
 
     @property
     def last_pos(self):
-        "Property: last position of the buffer (len(data) - 1)."
+        """
+        Get the last accessible position in the buffer.
+
+        Returns:
+            int of the last indexable position.
+        """
         return self._last_pos
 
     @property
-    def data(self):
-        "Property: raw data."
+    def data(self):  # noqa: WPS110
+        """
+        Get the raw data.
+
+        Returns:
+            raw data.
+        """
         return self._data
 
     def __len__(self):
+        """
+        Get the length computed during initialization.
+
+        Returns:
+            length of the buffer.
+        """
         return self._data_len
 
     @property
     def pchar(self):
-        "Property: previous character / neighboring character to the left."
+        """
+        Get the previous / left-neighboring character.
+
+        Returns:
+            left-neighboring character.
+        """
         return self._pchar
 
     @property
     def char(self):
-        "Property: current character."
+        """
+        Get the current character.
+
+        Returns:
+            current character.
+        """
         return self._char
 
     @property
     def nchar(self):
-        "Property: next character / neighboring character to the right."
+        """
+        Get the next / right-neighboring character.
+
+        Returns:
+            right-neighboring character.
+        """
         return self._nchar
 
     def move(self, pos: int):
