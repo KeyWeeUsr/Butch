54a8347b89a1a2fd01da8818af93f579a96c9f8f
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2021-07-28 18:48:17 +0200

    [REF] Lint tokens.py

--- butch/tokens.py
+++ butch/tokens.py
@@ -2,36 +2,69 @@
 
 
 class Argument:
-    "Token holding the raw value of an argument and its properties."
+    """Token holding the raw value of an argument and its properties."""
 
-    _value: str = ""
-    _quoted: bool = False
+    _value: str  # noqa: WPS110
+    _quoted: bool
 
-    def __init__(self, value: str, quoted: bool = False):
-        self._value = value
+    def __init__(self, value: str, quoted: bool = False):  # noqa: WPS110
+        """
+        Initialize Argument instance.
+
+        Args:
+            value (str): raw argument value to store
+            quoted (bool): flag if the value is encapsulated in quotes
+        """
+        self._value = value  # noqa: WPS110
         self._quoted = quoted
 
     @property
-    def value(self):
-        "Property: raw argument value."
+    def value(self):  # noqa: WPS110
+        """
+        Raw raw argument value.
+
+        Returns:
+            whatever was stored
+        """
         return self._value
 
     @value.setter
-    def value(self, value):
-        self._value = value
+    def value(self, value):  # noqa: WPS110
+        self._value = value  # noqa: WPS110
 
     @property
     def quoted(self):
-        "Property: argument value is quoted."
+        """
+        Get if the Argument is quoted.
+
+        Returns:
+            boolean
+        """
         return self._quoted
 
     def __repr__(self):
-        val = self._value
-        if self.quoted:
-            val = repr(val)
-        return f"<Argument: {val!r}, q={int(self.quoted)}>"
+        """
+        Get a string representation of Argument instance.
+
+        Returns:
+            string representation
+        """
+        stored = self._value
+        quoted = int(self.quoted)
+        if quoted:
+            stored = repr(stored)
+        return f"<Argument: {stored!r}, q={quoted}>"
 
     def __eq__(self, other):
+        """
+        Check if two Argument instances have the same content.
+
+        Args:
+            other (Any): value to compare current instance with
+
+        Returns:
+            boolean for content equality
+        """
         if not isinstance(other, Argument):
             return False
         return self.value == other.value
