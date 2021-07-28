805b8cf255da95abd0039cd3b3a43a44b6c60bab
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2021-07-28 18:44:07 +0200

    [REF] Lint outputs.py

--- butch/outputs.py
+++ butch/outputs.py
@@ -1,26 +1,41 @@
-"Module for output containers if simple print() or return isn't enough."
+"""Module for output containers if simple print() or return isn't enough."""
 from io import StringIO
 
 
 class CommandOutput:
-    "Container for STDOUT and STDERR buffers for piping and redirection."
+    """Container for STDOUT and STDERR buffers for piping and redirection."""
 
-    _stdout: StringIO = None
-    _stderr: StringIO = None
+    _stdout: StringIO
+    _stderr: StringIO
 
     def __init__(self, stdout: bool = True, stderr: bool = False):
+        """
+        Initialize STDOUT and/or STDERR buffers based on params.
+
+        Args:
+            stdout (bool): should create STDOUT buffer
+            stderr (bool): should create STDERR buffer
+        """
         # ask to allocate, don't provide custom though
-        if stdout:
-            self._stdout = StringIO()
-        if stderr:
-            self._stderr = StringIO()
+        self._stdout = StringIO() if stdout else None
+        self._stderr = StringIO() if stderr else None
 
     @property
     def stdout(self):
-        "Property: standard output buffer."
+        """
+        Get the standard output buffer.
+
+        Returns:
+            StringIO buffer.
+        """
         return self._stdout
 
     @property
     def stderr(self):
-        "Property: standard error buffer."
+        """
+        Get the standard error buffer.
+
+        Returns:
+            StringIO buffer.
+        """
         return self._stderr
