ba7f8324b08bdde445370fe651b5930e81dd546d
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2021-07-28 19:13:48 +0200

    [REF] Lint charlist.py

--- butch/charlist.py
+++ butch/charlist.py
@@ -1,40 +1,74 @@
 """
-Mix between a string and an array to behave like a buffer, but without
-guessing the buffer.seek(N) number or tracking it, so it can be passed
-between functions as a mutable parameter.
+Mix between a string and an array to behave like a buffer.
+
+For passing between functions as a mutable parameter without guessing
+the buffer.seek(N) number or even tracking it out of the container.
 """
 
 
 class CharList:
     """
-    Because immutable strings when passed between functions don't help much
-    for the tokenizer module.
+    Make a string mutable.
+
+    Used to pass strings between functions to prevent repetition.
     """
 
-    _data: str = ""
+    _data: str  # noqa: WPS110
 
-    def __init__(self, data: str = ""):
-        self._data = data
+    def __init__(self, data: str = ""):  # noqa: WPS110
+        """
+        Initialize CharList instance.
+
+        Args:
+            data (str): string to store
+        """
+        self._data = data  # noqa: WPS110
 
     def __iadd__(self, other: str):
-        self._data += other
+        """
+        Implement += operator.
+
+        Args:
+            other (str): string to append
+
+        Returns:
+            this instance
+        """
+        self._data += other  # noqa: WPS110
         return self
 
     @property
-    def data(self):
-        "Return the raw stored data."
+    def data(self):  # noqa: WPS110
+        """
+        Return the raw stored data.
+
+        Returns:
+            stored string
+        """
         return self._data
 
     @data.setter
-    def data(self, value: str):
-        self._data = value
+    def data(self, value: str):  # noqa: WPS110
+        self._data = value  # noqa: WPS110
 
     def __repr__(self):
+        """
+        Get a string representation of the instance.
+
+        Returns:
+            string representation
+        """
         return f"CharList({self._data!r})"
 
     def __bool__(self):
+        """
+        Booleanize the stored data.
+
+        Returns:
+            boolean
+        """
         return bool(self._data)
 
     def clear(self):
-        "Empty the buffer."
-        self._data = ""
+        """Empty the buffer."""
+        self._data = ""  # noqa: WPS110
