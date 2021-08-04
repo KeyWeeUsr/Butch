1a1f7c5d8f1e1843aa664e25d73f2cbf3985b392
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2021-08-04 18:09:25 +0200

    [REF] Split commands tests into separate classes

--- butch/tests/test_commands.py
+++ butch/tests/test_commands.py
@@ -8,7 +8,7 @@ from unittest import main, TestCase
 from unittest.mock import patch, MagicMock
 
 
-class Commands(TestCase):
+class Common(TestCase):
     def test_loggging_decorator_nolog(self):
         from butch.commands import what_func, LOG_STR
         from butch.context import Context
@@ -55,6 +55,8 @@ class Commands(TestCase):
         self.assertEqual(dummy(ctx=ctx), dummy_value)
         mocked.assert_called_once_with(LOG_STR, func_name, None, ctx)
 
+
+class EchoCommand(TestCase):
     def test_echo_plain_single(self):
         import sys
         from butch.context import Context
