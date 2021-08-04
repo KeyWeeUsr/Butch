7f635b64600c064b4ef4b9a0b0d0a05df4b5a18e
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2021-08-04 19:12:25 +0200

    [ADD] Add test for folder multiple args in type command

--- butch/tests/test_commands.py
+++ butch/tests/test_commands.py
@@ -262,6 +262,29 @@ class TypeCommand(TestCase):
         with open(__file__) as this_file:
             self.assertEqual(pipe.read(), FILE_NOT_FOUND + "\n")
 
+    def test_type_folder_multi(self):
+        import sys
+        from os.path import abspath, dirname
+        from butch.context import Context
+        from butch.commands import type_cmd
+        from butch.constants import ACCESS_DENIED, ERROR_PROCESSING
+        from butch.tokens import Argument
+
+        dummy = "dummy"
+
+        ctx = Context()
+        ctx.collect_output = True
+        path = dirname(abspath(__file__))
+        type_cmd(params=[Argument(value=path)] * 2, ctx=ctx)
+
+        pipe = ctx.output.stdout
+        self.assertEqual(pipe.read(), "")  # no seek
+        pipe.seek(0)
+        error = ERROR_PROCESSING.format(path)
+        self.assertEqual(
+            pipe.read(), f"{path}\n{ACCESS_DENIED}\n{error}\n" * 2
+        )
+
     def test_type_nonexisting_multi(self):
         import sys
         from os.path import abspath, dirname
