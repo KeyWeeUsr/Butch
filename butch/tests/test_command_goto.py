aa2b66c31f4481fd4af966540aaa8a494fef3a9f
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2021-08-11 01:27:32 +0200

    [ADD] Add test for goto command

---
+++ butch/tests/test_command_goto.py
@@ -0,0 +1,41 @@
+# the value from locals will be removed, which is desired
+# pylint: disable=import-outside-toplevel
+# pylint: disable=missing-function-docstring,missing-class-docstring
+# pylint: disable=missing-module-docstring
+# pylint: disable=too-many-lines,too-many-locals
+
+from unittest import main, TestCase
+from unittest.mock import patch, MagicMock
+
+
+class GotoCommand(TestCase):
+    def test_goto_help(self):
+        import sys
+        from butch.context import Context
+        from butch.commands import goto
+        from butch.tokens import Argument
+        from butch.constants import PARAM_HELP
+        from butch.commandtype import CommandType
+
+        ctx = Context()
+        with patch("butch.commands.print_help") as prnt:
+            goto(params=[Argument(value=PARAM_HELP)], ctx=ctx)
+        prnt.assert_called_once_with(cmd=CommandType.GOTO, file=sys.stdout)
+        self.assertEqual(ctx.error_level, 0)
+
+        ctx.collect_output = True
+        with patch("butch.commands.print_help") as prnt:
+            goto(params=[Argument(value=PARAM_HELP)], ctx=ctx)
+        prnt.assert_called_once_with(
+            cmd=CommandType.GOTO, file=ctx.output.stdout
+        )
+        self.assertEqual(ctx.error_level, 0)
+
+    def test_goto_without_label(self):
+        from butch.context import Context
+        from butch.commands import goto
+
+        ctx = Context()
+        self.assertEqual(ctx.error_level, 0)
+        self.assertIsNone(goto(params=[], ctx=ctx))
+        self.assertEqual(ctx.error_level, 1)
