c445bb378daefcb029be886b62399824afd1e3c8
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2021-08-11 18:38:29 +0200

    [REF] Add optional pattern for test names

--- test.sh
+++ test.sh
@@ -1,10 +1,12 @@
 #!/bin/sh -e
 PKG="butch"
+PATTERN=${PATTERN:-'test*.py'}
 coverage run \
     --branch \
     --source $PKG \
     --module unittest \
         discover \
+        --pattern $PATTERN \
         --failfast \
         --catch \
         --start-directory $PKG \
