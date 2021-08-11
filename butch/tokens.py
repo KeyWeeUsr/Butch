7a57cf338b20e8f49cdb534268d14917cf6238f8
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2021-08-11 02:34:43 +0200

    [REF] Add base value class for common Token properties

--- butch/tokens.py
+++ butch/tokens.py
@@ -1,10 +1,61 @@
 """Module for storing the tokens for tokenizer."""
 
 
-class Argument:
-    """Token holding the raw value of an argument and its properties."""
+class BaseValue:
+    """Base for a token holding the raw value with repr() and == operator."""
 
     _value: str  # noqa: WPS110
+
+    def __init__(self, value: str):  # noqa: WPS110
+        """
+        Initialize BaseValue instance.
+
+        Args:
+            value (str): raw value to store
+        """
+        self._value = value  # noqa: WPS110
+
+    @property
+    def value(self):  # noqa: WPS110
+        """
+        Raw value.
+
+        Returns:
+            whatever was stored
+        """
+        return self._value
+
+    @value.setter
+    def value(self, value):  # noqa: WPS110
+        self._value = value  # noqa: WPS110
+
+    def __repr__(self):
+        """
+        Get a string representation of this instance.
+
+        Returns:
+            string representation
+        """
+        return f"<{self.__class__.__name__}: {self._value!r}>"
+
+    def __eq__(self, other):
+        """
+        Check if two instances have the same content.
+
+        Args:
+            other (Any): value to compare current instance with
+
+        Returns:
+            boolean for content equality
+        """
+        if not isinstance(other, BaseValue):
+            return False
+        return self.value == other.value
+
+
+class Argument(BaseValue):
+    """Token holding the raw value of an argument and its properties."""
+
     _quoted: bool
 
     def __init__(self, value: str, quoted: bool = False):  # noqa: WPS110
@@ -15,22 +66,8 @@ class Argument:
             value (str): raw argument value to store
             quoted (bool): flag if the value is encapsulated in quotes
         """
-        self._value = value  # noqa: WPS110
+        super().__init__(value=value)
         self._quoted = quoted
-
-    @property
-    def value(self):  # noqa: WPS110
-        """
-        Raw raw argument value.
-
-        Returns:
-            whatever was stored
-        """
-        return self._value
-
-    @value.setter
-    def value(self, value):  # noqa: WPS110
-        self._value = value  # noqa: WPS110
 
     @property
     def quoted(self):
@@ -54,17 +91,3 @@ class Argument:
         if quoted:
             stored = repr(stored)
         return f"<Argument: {stored!r}, q={quoted}>"
-
-    def __eq__(self, other):
-        """
-        Check if two Argument instances have the same content.
-
-        Args:
-            other (Any): value to compare current instance with
-
-        Returns:
-            boolean for content equality
-        """
-        if not isinstance(other, Argument):
-            return False
-        return self.value == other.value
